5ca4ad8f9a3d82a48021676f476242a8bf441c7b
Marcus Baer <marcus.baer@publicispixelpark.de>
2022-06-15 01:24:35 +0200

    locations

--- components/locations-map.js
+++ components/locations-map.js
@@ -3,6 +3,10 @@ export default class LocationsMap extends HTMLElement {
     return 'locations-map'
   }
 
+  static get observedAttributes () {
+    return ['locations']
+  }
+
   constructor () {
     super()
 
@@ -12,16 +16,42 @@ export default class LocationsMap extends HTMLElement {
     `
     this.loadStyles(['../vendor/leaflet/leaflet.css'])
 
+    const defaultView = [51.5, -0.09]
+    const customView = this.getAttribute('view')
+    const view = customView? customView.split(','): defaultView
     this.mapNode = this.shadow.querySelector('#map')
-    this.map = L.map(this.mapNode).setView([51.505, -0.09], 13)
+    this.map = L.map(this.mapNode).setView(view, this.getAttribute('zoom') || 13)
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map)
 
-    L.marker([51.5, -0.09]).addTo(this.map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup()
+    // L.marker([51.5, -0.09]).addTo(this.map)
+    //   .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    //   .openPopup()
+  }
+
+  attributeChangedCallback(name, oldVal, newVal) {
+    this[name] = newVal;
+  }
+
+  set locations (val = '') {
+    const locations = JSON.parse(val)
+    locations.forEach(location => {
+      const customIcon = L.icon({
+        iconUrl: location.type? `img/${location.type}.svg`: 'icon.svg',
+        iconSize: [30, 30],
+        iconAnchor: [0, 0],
+        popupAnchor: [15, 0],
+      })
+
+      L.marker([location.lat, location.lon], {icon: customIcon}).addTo(this.map)
+        .bindPopup(`
+          <h3>${location.info}</h3>
+          <p>${location.address || ''}</p>
+          <p>${location.phone || ''} <a href="${location.web || ''}" target="_blank">${location.web || ''}</a></p>
+        `)
+    })
   }
 
   async loadStyles (optionalStyles = []) {
