5cf9e42a2865aa5f5a52a0830ebb9ba8e02e7515
Marcus Baer <marcus.baer@publicispixelpark.de>
2022-06-16 01:19:32 +0200

    sw cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const RELEASE_ID = '0.0.2'
+const RELEASE_ID = '0.0.3'
 const CACHE_NAME = `camping-cache-v${RELEASE_ID}`
 
 self.addEventListener('install', event => {
@@ -10,11 +10,31 @@ self.addEventListener('install', event => {
         cache.addAll([
           './',
           './index.html',
-          './share.html',
           './icon_512x512.png',
           './icon.svg',
           './manifest.json',
-          './modules/pwa.js'
+          './modules/pwa.js',
+          './components/critical.js',
+          './components/essential.js',
+          './components/essential-element.css',
+          './components/essential-element.js',
+          './components/image-list.css',
+          './components/image-list.js',
+          './components/locations-map.css',
+          './components/locations-map.js',
+          './components/md-content.css',
+          './components/md-content.js',
+          './components/praizee-slides.css',
+          './components/praizee-slides.js',
+          './components/qr-scanner.css',
+          './components/qr-scanner.js',
+          './cms.html',
+          './equipment/faltcaravan.md',
+          './map.html',
+          './qr.html',
+          './share.html',
+          './demo.html',
+          './details.html',
         ])
       )
   )
